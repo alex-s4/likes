abce57d9f565f08b474cfc4e32c9169ae4b0cc4a
alex-s4 <al39726867@gmail.com>
2023-02-15 07:41:06 +0800

    first attempt in code fixing

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 const likeButton = document.querySelectorAll(".like-btn")
 const likeCountDisplay = document.querySelectorAll(".like-count")
-var likesCount = [9,0,0]
+var likesCount2 = [0,0,0]
 
 
 likeButton[0].onclick = function() {
-    likesCount[0]++;
-    likeCountDisplay[0].innerText = likesCount[0]
+    likesCount2[0]++;
+    likeCountDisplay[0].innerText = likesCount2[0]
 }
 
 likeButton[1].onclick = function() {
-    likesCount[1]++;
-    likeCountDisplay[1].innerText = likesCount[1]
+    likesCount2[1]++;
+    likeCountDisplay[1].innerText = likesCount2[1]
 }
 
 likeButton[2].onclick = function() {
-    likesCount[2]++;
-    likeCountDisplay[2].innerText = likesCount[2]
+    likesCount2[2]++;
+    likeCountDisplay[2].innerText = likesCount2[2]
 }
