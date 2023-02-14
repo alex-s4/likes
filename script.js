632352b3cfc01946f38ba34b9953c35ad2d6aaeb
alex-s4 <al39726867@gmail.com>
2023-02-14 22:40:56 +0800

    add multiple cards with same like function

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-const likeButton = document.querySelector(".like-btn")
-const likeCountDisplay = document.querySelector(".like-count")
-var likesCount = 0
+const likeButton = document.querySelectorAll(".like-btn")
+const likeCountDisplay = document.querySelectorAll(".like-count")
+var likesCount = [0,0,0]
 
-likeButton.onclick = function() {
-    likesCount++;
-    likeCountDisplay.innerText = likesCount
+
+likeButton[0].onclick = function() {
+    likesCount[0]++;
+    likeCountDisplay[0].innerText = likesCount[0]
+}
+
+likeButton[1].onclick = function() {
+    likesCount[1]++;
+    likeCountDisplay[1].innerText = likesCount[1]
+}
+
+likeButton[2].onclick = function() {
+    likesCount[2]++;
+    likeCountDisplay[2].innerText = likesCount[2]
 }
